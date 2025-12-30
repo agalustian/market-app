f2c7e464950b220d4df63ddca0832171ac7b196e
agalustian <agalustian@yahoo.com>
2025-12-30 23:55:46 +0300

    feat (sprint-6): rework order models and dto, add reactive repository

--- src/main/java/ru/market/dto/OrderDTO.java
+++ src/main/java/ru/market/dto/OrderDTO.java
@@ -2,11 +2,12 @@ package ru.market.dto;
 
 import java.util.List;
 import ru.market.models.Order;
+import ru.market.models.OrderItem;
 
 public record OrderDTO(Integer id, List<OrderItemDTO> items, Integer totalSum) {
 
-  public static OrderDTO from(Order order) {
-    List<OrderItemDTO> orderItemsDTO = order.getOrderItems().stream().map(OrderItemDTO::from).toList();
+  public static OrderDTO from(Order order, List<OrderItem> orderItems) {
+    List<OrderItemDTO> orderItemsDTO = orderItems.stream().map(OrderItemDTO::from).toList();
 
     return new OrderDTO(order.getId(), orderItemsDTO, order.getTotalSum());
   }
