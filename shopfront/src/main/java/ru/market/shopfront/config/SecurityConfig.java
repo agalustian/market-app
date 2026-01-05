06b77a5d6a3fb6024b1642fe392175559bc4d95e
agalustian <agalustian@yahoo.com>
2026-01-05 14:42:16 +0300

    feat (sprint-8): add security config

---
+++ shopfront/src/main/java/ru/market/shopfront/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package ru.market.shopfront.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.net.URI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.WebSessionServerCsrfTokenRepository;
+
+// TODO replace in-memory storage to keycloak
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public WebSessionServerCsrfTokenRepository csrfTokenRepository() {
+    return new WebSessionServerCsrfTokenRepository();
+  }
+
+  @Bean
+  public MapReactiveUserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("user")
+        .password(passwordEncoder().encode("password"))
+        .roles("USER")
+        .build();
+    return new MapReactiveUserDetailsService(user);
+  }
+
+  @Bean
+  public RedirectServerLogoutSuccessHandler redirectServerLogoutSuccessHandler() {
+    RedirectServerLogoutSuccessHandler logoutSuccessHandler = new RedirectServerLogoutSuccessHandler();
+    logoutSuccessHandler.setLogoutSuccessUrl(URI.create("/items"));
+    return logoutSuccessHandler;
+  }
+
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+                                                          RedirectServerLogoutSuccessHandler redirectServerLogoutSuccessHandler) {
+    http
+        .csrf(csrf -> csrf
+            .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+        )
+        .authorizeExchange(exchanges -> exchanges
+            .pathMatchers("/items").permitAll()
+            .anyExchange().authenticated()
+        )
+        .formLogin(form -> form
+            .loginPage("/login")
+            .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/items"))
+        )
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessHandler(redirectServerLogoutSuccessHandler)
+        )
+        .oauth2Client(withDefaults());
+    return http.build();
+  }
+
+}
